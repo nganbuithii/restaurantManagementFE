7b11bfbed26e43aa717811593bf22b022e4fb8af
Ngân <nganbui.23112003@gmail.com>
2024-08-08 12:10:08 +0700

    UI order - cart

--- components/footer.tsx
+++ components/footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
             </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-4 text-center text-gray-400">
-            <p>&copy; 2024 Design by NganBuiThi</p>
+            <p>&copy; 2024 Design by NganBuiThi - information technology industry project</p>
         </div>
     </footer>
     )};
